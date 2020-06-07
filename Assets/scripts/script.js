9d7fa883fc882ccdef384986ce9c8b686f4b2d0d
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-06-07 15:20:09 -0400

    made some edits to the JS file

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,24 @@
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var interval;
+
+
 // Assignment Code
 var startBtn = document.querySelector("#start");
 console.log("Javascript");
+
+
+function startQuiz () {
+
+// Timer 
+
+var x = set
+
+
+
+}
+
+
+// Add event listener to the "Start the Quiz!" button 
+
+startBtn.addEventListener("click", startQuiz);
